9a3dd478b46d57eb74cc62e501bce6e24f967cfc
prakashkambli <howtocreate2021@gmail.com>
2025-02-26 00:38:18 +0530

    Fixed Division

--- calculator.py
+++ calculator.py
@@ -11,7 +11,10 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
-        return a / b
+      if b == 0:
+          raise ValueError("Cannot divide by zero.")
+      return a / b
+
 
     # TODO: Implement the following function to calculate the square root of a number.
     def square_root(self, x):
@@ -31,3 +34,5 @@ if __name__ == "__main__":
     # Uncomment and test the square root feature
     num3 = 25
     print(f"The square root of {num3} = {calculator.square_root(num3)}")
+            
+    
